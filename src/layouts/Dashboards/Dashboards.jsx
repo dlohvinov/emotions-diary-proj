ca5f6c651c234a926c04f423aae8f2292431df70
dlohvinov <nsblnr@gmail.com>
2023-10-14 17:20:20 +0300

    feat: feelings count donut dashboard

--- src/layouts/Dashboards/Dashboards.jsx
+++ src/layouts/Dashboards/Dashboards.jsx
@@ -1,21 +1,30 @@
 import { Box, Typography } from '@mui/joy';
+import { useTranslation } from 'react-i18next';
+import FeelingsDonutChart from './FeelingsDonutChart.jsx';
 
 function Dashboards() {
+  const { t } = useTranslation();
+
   return (
     <Box
+      component="section"
       sx={{
         height: '100%',
         minHeight: 0,
         display: 'flex',
         p: 2,
-        justifyContent: 'center',
-        alignItems: 'center',
         bgcolor: 'background.surface',
         boxShadow: 'md',
+        flexDirection: 'column',
+        gap: 2,
       }}
     >
-      <Typography>Dashboards</Typography>
+      <Typography level="h2">
+        {t('DASHBOARDS')}
+      </Typography>
+      <FeelingsDonutChart />
     </Box>
+
   );
 }
 
---
+++ src/layouts/Dashboards/FeelingsDonutChart.jsx
@@ -0,0 +1,75 @@
+import { Card, Title, DonutChart } from '@tremor/react';
+import {
+  collection, getDoc,
+  getFirestore,
+  onSnapshot,
+  query,
+  where,
+} from 'firebase/firestore';
+import { useSelector } from 'react-redux';
+import { useEffect, useState, useMemo } from 'react';
+import { useTranslation } from 'react-i18next';
+import { userinfoSelector } from '../../features/auth/authSlice.js';
+import { instanceSelector } from '../../features/firebase/firebaseSlice.js';
+
+function FeelingsDonutChart() {
+  const { t } = useTranslation();
+
+  const userinfo = useSelector(userinfoSelector);
+  const instance = useSelector(instanceSelector);
+  const db = getFirestore(instance);
+
+  const [rawData, setRawData] = useState([]);
+
+  useEffect(() => {
+    if (!userinfo) return;
+    const q = query(collection(db, 'logs'), where('uid', '==', userinfo.uid));
+    const unsubscribe = onSnapshot(q, async (docSnapshot) => {
+      const records = [];
+      for (const doc of docSnapshot.docs) {
+        const feelings = [];
+        for (const feeling of doc.data().feelings) {
+          feelings.push((await getDoc(feeling)).data());
+        }
+        const record = {
+          feelings,
+        };
+
+        records.push(record);
+      }
+      setRawData(records);
+    });
+
+    return unsubscribe;
+  }, [userinfo]);
+
+  const aggFeelingsCount = useMemo(() => {
+    const map = rawData.reduce((acc, cur) => {
+      cur.feelings.forEach((feeling) => {
+        if (acc[feeling.name]) {
+          acc[feeling.name] += 1;
+        } else {
+          acc[feeling.name] = 1;
+        }
+      });
+      return acc;
+    }, {});
+    return Object.entries(map).map(([name, count]) => ({ name, count }));
+  }, [rawData]);
+
+
+  return (
+    <Card className="max-w-lg">
+      <Title>{t('Feelings count')}</Title>
+      <DonutChart
+        className="mt-6"
+        data={aggFeelingsCount}
+        category="count"
+        index="name"
+        colors={['slate', 'violet', 'indigo', 'rose', 'cyan', 'amber']}
+      />
+    </Card>
+  );
+}
+
+export default FeelingsDonutChart;
